88b33c53fb7e808e2e375962615b859bc2674d8c
bodya18 <bodya18x@mail.ru>
2021-10-18 14:14:09 +0300

    collation

--- models/tables.py
+++ models/tables.py
@@ -7,37 +7,44 @@ meta = MetaData()
 voices = Table(
     'voices', meta,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('voice', String(255), nullable=False, unique=True),
+    Column('voice', String(255, collation="utf8mb4_bin"), nullable=False, unique=True)
 )
 
 serials = Table(
     'serials', meta,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('title', String(255), nullable=False, unique=True)
+    Column('title', String(255, collation="utf8mb4_bin"), nullable=False, unique=True)
 )
 
 seasons = Table(
     'seasons', meta,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('title', String(255), nullable=False),
-    Column('description', TEXT, nullable=False),
+    Column('title', String(255, collation="utf8mb4_bin"), nullable=False),
+    Column('description', TEXT(collation="utf8mb4_bin"), nullable=False),
     Column('number', Integer),
     Column('serialId', Integer, ForeignKey('serials.id')),
-    Column('link', String(255)),
-    Column('image', String(255), nullable=False),
+    Column('link', String(255, collation="utf8mb4_bin")),
+    Column('image', String(255, collation="utf8mb4_bin"), nullable=False)
 )
 
 episodes = Table(
     'episodes', meta,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('title', String(255)),
+    Column('title', String(255, collation="utf8mb4_bin")),
     Column('seazonId', Integer, ForeignKey('seasons.id')),
     Column('voiceId', Integer, ForeignKey('voices.id')),
     Column('number', Integer, nullable=False),
-    Column('link', String(255), nullable=False),
-    Column('subtitles', String(255))
+    Column('link', String(255, collation="utf8mb4_bin"), nullable=False),
+    Column('subtitles', String(255, collation="utf8mb4_bin"))
 )
 
 meta.create_all(engine)
 
 CONNECTION = engine.connect()
+result = voices.select()
+result = CONNECTION.execute(result).fetchall()
+if(result == []):
+    data = ['','HDRezka', 'BaibaKo','FOX','LostFilm','NewStudio','AniDub','Оригинал','Пифагор','Субтитры','кубик в кубе']
+    for i in data:
+        ins = voices.insert().values(voice = i) 
+        CONNECTION.execute(ins)
